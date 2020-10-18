cdf77ca780991a3685f8f2c3135a6953c5cac83f
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-19 03:18:26 +0530

    feat: added tags to models

--- src/models/models.go
+++ src/models/models.go
@@ -2,17 +2,17 @@ package models
 
 // Participant is a struct defining the participant object
 type Participant struct {
-	Name  string
-	Email string
-	RSVP  string
+	Name  string `bson:"name" json:"name"`
+	Email string `bson:"email" json:"email"`
+	RSVP  string `bson:"rsvp" json:"rsvp"`
 }
 
 // Meeting is a struct defining the meeting object
 type Meeting struct {
-	ID           string
-	Title        string
-	Participants []Participant
-	StartTime    int64
-	EndTime      int64
-	CreatedAt    int64
+	ID           string        `bson:"id" json:"id"`
+	Title        string        `bson:"title" json:"title"`
+	Participants []Participant `bson:"participants" json:"participants"`
+	StartTime    int64         `bson:"startTime" json:"startTime"`
+	EndTime      int64         `bson:"endTime" json:"endTime"`
+	CreatedAt    int64         `bson:"createdAt" json:"createdAt"`
 }
