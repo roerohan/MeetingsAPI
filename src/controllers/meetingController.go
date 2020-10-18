83914113db0236c4fe1941382017e0d5184e4868
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-19 04:50:51 +0530

    feat: Added meetingController.go

--- src/controllers/meetingController.go
+++ src/controllers/meetingController.go
@@ -1,9 +1,41 @@
 package controllers
 
 import (
-	"go.mongodb.org/mongo-driver/mongo"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/roerohan/MeetingsAPI/src/utils"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-type RouteHandler struct {
-	Meeting *mongo.Collection
+func (router *RouteHandler) getMeetingByID(w http.ResponseWriter, r *http.Request) {
+	id := utils.ExtractParam(r)
+
+	var meeting bson.M
+
+	err := router.Meeting.FindOne(context.TODO(), bson.D{{
+		"id", id,
+	}}).Decode(&meeting)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(meeting)
+}
+
+// MeetingController handles requests to /meetings
+func (router *RouteHandler) MeetingController(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		router.getMeetingByID(w, r)
+
+	default:
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
