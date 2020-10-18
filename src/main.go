f97270ac3d3b7491581c932e4059c0c045f910b0
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-19 04:56:10 +0530

    fix: fmt.Println -> log.Println

--- src/main.go
+++ src/main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 
-	"github.com/roerohan/MeetingsAPI/src/models"
 	"github.com/roerohan/MeetingsAPI/src/controllers"
+	"github.com/roerohan/MeetingsAPI/src/models"
 )
-
 
 func httpListener(port int) {
 	portStr := strconv.Itoa(port)
 
 	client := models.MongoConnect("mongodb://localhost:27017")
 	router := controllers.NewRouteHandler(client.Database("MeetingsAPI").Collection("meetings"))
-	
+
 	http.HandleFunc("/meetings", router.MeetingsController)
 	http.HandleFunc("/meeting/", router.MeetingController)
 
-	fmt.Println("[INFO] Listening on PORT " + portStr)
+	log.Println("[INFO] Listening on PORT " + portStr)
 	log.Fatal(http.ListenAndServe(":"+portStr, nil))
 }
-
 
 func main() {
 	httpListener(3000)
