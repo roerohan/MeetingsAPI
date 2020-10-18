3ff1f2c860a9f020bf032a22ec3258dbbe5679b0
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-19 04:51:51 +0530

    feat: Added meeting router

--- src/main.go
+++ src/main.go
@@ -15,10 +15,10 @@ func httpListener(port int) {
 	portStr := strconv.Itoa(port)
 
 	client := models.MongoConnect("mongodb://localhost:27017")
-	router := routes.NewRouteHandler(client.Database("MeetingsAPI").Collection("meetings"))
+	router := controllers.NewRouteHandler(client.Database("MeetingsAPI").Collection("meetings"))
 	
 	http.HandleFunc("/meetings", router.MeetingsController)
-	http.HandleFunc("/meeting", router.MeetingController)
+	http.HandleFunc("/meeting/", router.MeetingController)
 
 	fmt.Println("[INFO] Listening on PORT " + portStr)
 	log.Fatal(http.ListenAndServe(":"+portStr, nil))
